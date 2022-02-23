9fc02b91410e80e6602416facd37f132c9522cc6
ATMJin <atmjin0912@protonmail.com>
2022-02-23 20:11:09 +0800

    更新gulp

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,35 @@ function includeHTML() {
             prefix: '@@',
             basepath: '@file'
         }))
-        .pipe(dest('dist'));
+        .pipe(dest('./dist'));
 };
-exports.html = series(includeHTML, mv_img);
+exports.html = series(includeHTML, mv_img, mv_js, mv_php);
 
 //watch files many times
 exports.w = function watchs() {
     watch(['src/html/*.html', 'src/html/**/*.html'], includeHTML);
+    watch(['src/js/*.*', 'src/js/**/*.*'], mv_js);
+    watch(['src/phps/*.*', 'src/phps/**/*.*'], mv_php);
+    watch(['src/assets/images/*.*', 'src/assets/images/**/*.*'], mv_img);
 }
+
+//  圖片搬家
+function mv_img() {
+    return src(['src/assets/images/*.*', 'src/assets/images/**/*.*']).pipe(dest('dist/assets/images'))
+}
+exports.mvimg = mv_img
+
+//  JS搬家
+function mv_js() {
+    return src(['src/js/*.*', 'src/js/**/*.*']).pipe(dest('dist/js'))
+}
+exports.mvjs = mv_js
+
+//  PHP搬家
+function mv_php() {
+    return src(['src/phps/*.*', 'src/phps/**/*.*']).pipe(dest('dist/phps'))
+}
+exports.mvphp = mv_php
 
 // JS檔壓縮
 const uglify = require('gulp-uglify');
@@ -45,7 +66,7 @@ exports.minjs = ugjs;
 const cleanCSS = require('gulp-clean-css');
 
 function cleanC() {
-    return src('src/assets/css/*.css') //來源
+    return src('dist/assets/css/*.css') //來源
         .pipe(cleanCSS()) // 壓縮
         .pipe(rename({
             extname: '.min.css'
@@ -55,12 +76,19 @@ function cleanC() {
 exports.mincss = cleanC;
 
 
-// // 合併css
-// var concat = require('gulp-concat');
-// function concatCss() {
-//     return src('src/assets/css/*.css').pipe(concat('all.css')).pipe(dest('dist/assets/css/all/'))
-// }
-// exports.allcss = concatCss
+// 合併css
+var concat = require('gulp-concat');
+
+function concatCss() {
+    return src('dist/assets/css/*.css').pipe(concat('all.css')).pipe(dest('dist/assets/css/all/'))
+}
+exports.allcss = concatCss
+
+// 同時壓縮JS、CSS
+exports.all = series(ugjs, cleanC);
+
+// 同時壓縮JS、CSS、img
+exports.min = series(ugjs, cleanC, min_images);
 
 
 // sass編譯
@@ -75,20 +103,12 @@ exports.mincss = cleanC;
 // exports.scss = sassstyle;
 
 
-// 同時壓縮JS、CSS
-exports.all = series(ugjs, cleanC)
-
-//  圖片搬家
-function mv_img() {
-    return src('src/assets/images/*/*.*').pipe(dest('dist/assets/images'))
-}
-exports.mvimg = mv_img
 
 //  圖片壓縮 （上線用）
 const imagemin = require('gulp-imagemin');
 
 function min_images() {
-    return src('src/assets/images/*.*')
+    return src('src/assets/images/**/*.*')
         .pipe(imagemin([
             imagemin.mozjpeg({
                 quality: 70,
@@ -96,6 +116,6 @@ function min_images() {
             })
             // 壓縮品質 quality越低 -> 壓縮越大 -> 品質越差 
         ]))
-        .pipe(dest('dist/assets/images'))
+        .pipe(dest('dist/assets/images/min'))
 }
 exports.img = min_images;
