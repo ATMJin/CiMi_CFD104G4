eb9806ee18bf34e13499e2797d4eb24c4b23bfb9
ATMJin <atmjin0912@protonmail.com>
2022-02-23 16:49:52 +0800

    新增gulp架構

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const {
 const fileinclude = require('gulp-file-include');
 
 //watch files once
-exports.html = function includeHTML() {
+function includeHTML() {
     return src('html/*.html')
         .pipe(fileinclude({
             prefix: '@@',
@@ -17,10 +17,11 @@ exports.html = function includeHTML() {
         }))
         .pipe(dest('./'));
 };
+exports.html = series(includeHTML, mv_img);
 
 //watch files many times
 exports.w = function watchs() {
-    watch(['html/*.html', 'html/**/*.html'], exports.html);
+    watch(['html/*.html', 'html/**/*.html'], includeHTML);
 }
 
 // JS檔壓縮
@@ -76,3 +77,25 @@ exports.mincss = cleanC;
 
 // 同時壓縮JS、CSS
 exports.all = series(ugjs, cleanC)
+
+//  圖片搬家
+function mv_img() {
+    return src('src/images/*.*').pipe(dest('dist/images'))
+}
+exports.mvimg = mv_img
+
+//  圖片壓縮 （上線用）
+const imagemin = require('gulp-imagemin');
+
+function min_images() {
+    return src('src/assets/images/*.*')
+        .pipe(imagemin([
+            imagemin.mozjpeg({
+                quality: 70,
+                progressive: true
+            })
+            // 壓縮品質 quality越低 -> 壓縮越大 -> 品質越差 
+        ]))
+        .pipe(dest('dist/assets/images'))
+}
+exports.img = min_images;
