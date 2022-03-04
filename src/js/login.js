8c50e742e788d3a83210d18a4d0e5a0453a5da5f
Celia-code <infinite010750@gmail.com>
2022-03-04 18:16:27 +0800

    更新登入註冊畫面(包含RWD)

--- src/js/login.js
+++ src/js/login.js
@@ -1,48 +1,42 @@
-/*功能一 : 點擊小眼睛, 顯示使用者輸入之密碼*/
-const pswField = document.querySelector('.form input[type="password"]');
-const toogleBtn = document.querySelector('.form .field i');
-
-toogleBtn.onclick = () => {
-    if (pswField.type == 'password') {
-        pswField.type = 'text';
-        toogleBtn.classList.add('active');
-    } else {
-        pswField.type = 'password';
-        toogleBtn.classList.remove('active');
-    }
-}
-
-/*功能二 : 使用者登入*/
-
-	
-
 /*登入*/
 const form = document.querySelector('.form form')
 const loginBtn = document.getElementById('login');
-console.log(loginBtn);
 
 form.onsubmit = (e) => { //取消form表單預設事件
     e.preventDefault();
 
 }
 
-function $id(id){
-	return document.getElementById(id);
+function $id(id) {
+    return document.getElementById(id);
 }
+let member={};
 
-loginBtn.onclick = () => {
-    //執行ajax
-    let xhr = new XMLHttpRequest() //產生XML物件
-    
+
+/*功能一 : 使用者登入*/
+function getMemInfo() {
+    let xhr = new XMLHttpRequest() //產生XHR物件
+
     xhr.onload = () => {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                let data = xhr.response;
-                console.log(data);
-                if (data === "success") { //帳密正確
-                    location.href = "chat.html";
+                let res = JSON.parse(xhr.response)
+                console.log(res);
+                
+                if (res.output === "success") { //帳密正確
+
+                    //登入的時候把頭像換掉
+                    let mem_head = document.querySelector("img[alt='會員']");
+                    mem_head.src = res.mem_head 
+
+                    //把後端PHP資料寫回memInfo
+                    memInfo = res
+
+                    //跳轉回首頁
+                    // location.href = "index.html";
+
                 } else { //帳號密碼錯誤/欄位有空
-                    alert(data);
+                    alert(res.noPerson);
                 }
             }
         }
@@ -51,22 +45,32 @@ loginBtn.onclick = () => {
     xhr.open('POST', 'phps/login.php', true)
     xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
     let data_info = `mem_id=${$id('mem_id').value}&mem_psw=${$id('mem_psw').value}`
-    // let formData = new FormData(form); //創建formData物件
     xhr.send(data_info); //送表單資料到phps/login.php
 }
 
 
 
-/*取消*/
-const accountField = document.querySelector('.form input[type="text"]');
-const cancelBtn = document.getElementById('cancel');
-console.log(accountField);
-cancelBtn.onclick = () => {
-    accountField.value = "";
-    pswField.value = "";
-    accountField.focus();
+function init() {
+
+
+    loginBtn.onclick = () => {
+    getMemInfo()
+        //執行ajax
+    console.log(memInfo);
+
+
+    }
+    
+
+//     /*取消*/
+//     const accountField = document.querySelector('.form input[type="text"]');
+//     const cancelBtn = document.getElementById('cancel');
+//     // console.log(accountField);
+//     cancelBtn.onclick = () => {
+//         accountField.value = "";
+//         pswField.value = "";
+//         accountField.focus();
+//     }
 }
-
-/*功能三 : 使用者點立即註冊後切到register表單*/
-
+window.addEventListener('load', init)
 
