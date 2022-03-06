402f17aa92c7d74c0702a84ef40e9e6db69c4eb4
ATMJin <atmjin0912@protonmail.com>
2022-03-06 09:18:38 +0800

    連結資料庫PHP及範例

---
+++ src/phps/example.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+try{
+  // require_once("../connect_cfd104g4.php"); // 上線時使用
+  require_once("connect.php");  // 開發時使用
+  
+  // SQL指令
+  $sql = "SELECT * FROM mem"; 
+  $member = $pdo -> prepare($sql);
+  // 執行
+  $member -> execute();
+
+  // 將取得的資料轉成陣列形式
+  $memberRows = $member->fetch(PDO::FETCH_ASSOC);
+  
+  // 將陣列轉成json格式後傳出去
+  echo json_encode($memberRows);
+
+
+  // PHP執行失敗時執行下面
+}catch(PDOException $e){
+  echo $e->getMessage();
+}
+?>
