37c6027146593b06b75248ae7736d1d11770dfa7
YiWen-chao <chaoyiwen7@gmail.com>
2022-03-11 17:40:52 +0800

    會員num連結cimi

--- src/phps/response.php
+++ src/phps/response.php
@@ -4,9 +4,10 @@ try {
 	//執行sql指令
 	$question_no = $_GET["question_no"];
 	$mem_ans = $_GET["mem_ans"];
+	$mem_no = $_GET["mem_no"];
 
     $sql = "INSERT INTO paring_player_ans(question_no,mem_no,paring_ans)
-            VALUES({$question_no},1,{$mem_ans});";
+            VALUES({$question_no},{$mem_no},{$mem_ans});";
     
 
 	if($pdo->exec($sql)){ //exec用於異動資料的時候
