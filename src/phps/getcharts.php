84744a66f93a7a3b3b89a634d1ffbf60881099b4
suanriz <suanriz@gmail.com>
2022-03-15 22:02:31 +0800

    排行榜撈資料

--- src/phps/getcharts.php
+++ src/phps/getcharts.php
@@ -5,19 +5,70 @@ try{
   require_once("connect_suan.php");  // 開發時使用
   
   switch($_GET["case"]){
-    case "chartsname":
+    case 1: // 發文數
         // SQL指令
-        $sql = "SELECT mem_name FROM mem"; 
+        $sql = "SELECT mem_id, mem_name,mem_head,COUNT(a.mem_no) AS mem_post
+        FROM mem m 
+        JOIN article a 
+        ON a.mem_no = m.mem_no
+        WHERE a.article_no IS NOT NULL
+        GROUP BY a.mem_no
+        ORDER BY mem_post DESC"; 
         // 執行
-        $charts_mem = $pdo -> query($sql);
+        $charts_account = $pdo -> query($sql);
         // // 將取得的資料轉成陣列形式
-        $charts_memRows = $charts_mem->fetchAll(PDO::FETCH_ASSOC);
+        $charts_accountRows = $charts_account->fetchAll(PDO::FETCH_ASSOC);
         // // 將陣列轉成json格式後傳出去
-        echo json_encode($charts_memRows);
+        echo json_encode($charts_accountRows);
         break;
 
-    case 2:
+    case 2: //喜歡數
+      // SQL指令
+      $sql = "SELECT mem_id, mem_name,mem_head,article_likes_amount
+      FROM mem m 
+      JOIN article a 
+      ON a.mem_no = m.mem_no
+      WHERE a.article_likes_amount IS NOT NULL
+      ORDER BY a.article_likes_amount DESC"; 
+      // 執行
+      $charts_account = $pdo -> query($sql);
+      // // 將取得的資料轉成陣列形式
+      $charts_accountRows = $charts_account->fetchAll(PDO::FETCH_ASSOC);
+      // // 將陣列轉成json格式後傳出去
+      echo json_encode($charts_accountRows);
+      break;
 
+    case 3: //收藏數
+        // SQL指令
+        $sql = "SELECT mem_id, mem_name,mem_head,article_collect_amount
+        FROM mem m 
+        JOIN article a 
+        ON a.mem_no = m.mem_no
+        WHERE a.article_collect_amount IS NOT NULL
+        ORDER BY a.article_collect_amount DESC"; 
+        // 執行
+        $charts_account = $pdo -> query($sql);
+        // // 將取得的資料轉成陣列形式
+        $charts_accountRows = $charts_account->fetchAll(PDO::FETCH_ASSOC);
+        // // 將陣列轉成json格式後傳出去
+        echo json_encode($charts_accountRows);
+        break;
+
+    case 4: //追蹤數
+        // SQL指令
+        $sql = "SELECT mem_id, mem_name,mem_head,writer_no
+        FROM trackwriter t
+        JOIN mem m 
+        ON t.writer_no = m.mem_no
+        WHERE t.writer_no IS NOT NULL
+        GROUP BY t.writer_no
+        ORDER BY t.writer_no DESC"; 
+        // 執行
+        $charts_account = $pdo -> query($sql);
+        // // 將取得的資料轉成陣列形式
+        $charts_accountRows = $charts_account->fetchAll(PDO::FETCH_ASSOC);
+        // // 將陣列轉成json格式後傳出去
+        echo json_encode($charts_accountRows);
         break;
   }
 }catch(PDOException $e){
