8b73624a01eced9a30fed440adceeaceec3e204d
ATMJin <atmjin0912@protonmail.com>
2022-03-19 10:06:19 +0800

    忘記改回我們的空四格規範了

--- src/js/member/member_myfriends_vue.js
+++ src/js/member/member_myfriends_vue.js
@@ -1,32 +1,32 @@
 new Vue({
-  el: "#mifriendApp",
-  data: {
-    friends: [],
-    number: 0,
-  },
-  computed: {
-    imgrandom() {
-      let r = Math.floor(Math.random() * 10)
-      return `https://picsum.photos/1000/300/?random=${r}`;
+    el: "#mifriendApp",
+    data: {
+        friends: [],
+        number: 0,
     },
-  },
-  methods: {
-    showFriend(no) {
-      this.number = no;
-      document.querySelector(".friend_lightbox").style.display = "block"
+    computed: {
+        imgrandom() {
+            let r = Math.floor(Math.random() * 10)
+            return `https://picsum.photos/1000/300/?random=${r}`;
+        },
     },
-    hiddenFriend() {
-      document.querySelector(".friend_lightbox").style.display = "none"
-    }
-  },
-  mounted() {
-    fetch(`phps/member/member_myfriends.php?mem_no=${sessionStorage.getItem('mem_no')}`)
-      // fetch(`phps/member/member_myfriends.php?mem_no=${sessionStorage.getItem('mem_no')}`)
-      .then(res => res.json())
-      // .then(res => console.log(res))
-      .then(res => this.friends = res)
-      .catch(err => console.log(err))
-  },
+    methods: {
+        showFriend(no) {
+            this.number = no;
+            document.querySelector(".friend_lightbox").style.display = "block"
+        },
+        hiddenFriend() {
+            document.querySelector(".friend_lightbox").style.display = "none"
+        }
+    },
+    mounted() {
+        fetch(`phps/member/member_myfriends.php?mem_no=${sessionStorage.getItem('mem_no')}`)
+            // fetch(`phps/member/member_myfriends.php?mem_no=${sessionStorage.getItem('mem_no')}`)
+            .then(res => res.json())
+            // .then(res => console.log(res))
+            .then(res => this.friends = res)
+            .catch(err => console.log(err))
+    },
 })
 
 Vue.config.devtools = true;
