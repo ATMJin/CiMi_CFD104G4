36bded3c3cd7025a96cb9c9cee170386fe628ed9
YiWen-chao <chaoyiwen7@gmail.com>
2022-03-20 18:20:28 +0800

    修改attendance、my_introduction

--- src/js/attendance.js
+++ src/js/attendance.js
@@ -1,3 +1,4 @@
+let mem_no = sessionStorage.getItem('mem_no');
 let attendances = document.querySelectorAll(".attendance_item", ".attendance_7days_item");
 let attendances_7days = document.querySelectorAll(".attendance_7days_item");
 let coins_bonus= document.querySelectorAll(".coins_bonus_item");
@@ -8,9 +9,59 @@ let txt =document.getElementsByClassName("txt_box");
 let close = document.querySelectorAll(".close_icon");
 let lightbox = document.querySelectorAll(".lightbox");
 let day_list = ['日', '一', '二', '三', '四', '五', '六'];
-let day  = new Date().getDay();
+let day = new Date().getDay();
+
+let now = new Date();
+
+let year = now.getFullYear();
+let month = now.getMonth() + 1;
+let date = now.getDate();
+let check_day = `${year}-${month}-${date}`;
+
+(function monthchange() {
+  month = (month < 10 ? '0' : '') + month;
+})();
+
+(function datechange() {
+  date = (date < 10 ? '0' : '') + date;
+})();
+
+
+
+
+
 
 // console.log('close');
+function if_show_attendence(){
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function(){
+        let attendence = JSON.parse(xhr.responseText);
+        console.log(xhr.responseText);
+        if (attendence.mem_lastsign == check_day && mem_no !== null) {
+
+            for(let i = 0; i<attendances.length; i++){
+            attendances[i].onclick = function(){
+                coins[i].style.display = "none";
+                txt_box[i].style.display = "none";
+                checks[i].style.display = "block";
+                };
+            }
+            attendances_7days[0].onclick = function(){
+            coins_bonus[0].style.display = "none";
+            txt_box[6].style.display = "none";
+            checks[6].style.display = "block";
+    }
+            
+        } else {
+            
+        }
+
+        }
+    xhr.open("get", `phps/attendence?mem_no=${sessionStorage.getItem('mem_no')}.php`, true);
+    xhr.send(null);
+}
+
+
 
 function close_lightbox(){
     lightbox[0].style.display= "none";
@@ -23,21 +74,10 @@ document.write('Today is 星期' + day_list[day]);
 
 
 
-function init(){
-
-    for(let i = 0; i<attendances.length; i++){
-        attendances[i].onclick = function(){
-            coins[i].style.display = "none";
-            txt_box[i].style.display = "none";
-            checks[i].style.display = "block";
-        };
-    }
-    attendances_7days[0].onclick = function(){
-        coins_bonus[0].style.display = "none";
-        txt_box[6].style.display = "none";
-        checks[6].style.display = "block";
-    }
-
+function init() {
+    
     close[0].onclick=close_lightbox;
 }
+
+
 window.addEventListener("load", init, false);
--- src/phps/member/member_my_introduction.php
+++ src/phps/member/member_my_introduction.php
@@ -1,21 +1,30 @@
 <?php
-  include_once "member_config.php";
+include_once "../connect_cimi.php";
 
+switch ($_GET["case"]) {
+  case 'null':
+    $sql = "SELECT * FROM mem join mempairdata on mempairdata.mem_no = mem.mem_no 
+            where mem.mem_no= {$_GET['mem_no']}";
 
-$sql = 
-"SELECT mem_birth,mem_sex,mem_sexuality,mem_relationship,mem_job,
-mem_school,mem_sign,mem_interests,mem_lovecountry,mem_exchange,
-mem_try ,mem.mem_name,mem.mem_head
-FROM mem join mempairdata on mempairdata.mem_no = mem.mem_no 
-where mem.mem_no= {$_GET['mem_no']}";
-  
-  //執行sql
-  $member = $pdo -> query($sql);
+    //執行sql
+    $member = $pdo->query($sql);
 
-  // 將取得的資料轉成陣列形式
-  $memberRows = $member->fetch(PDO::FETCH_ASSOC);
-  
-  // 將陣列轉成json格式後傳出去
-  echo json_encode($memberRows);
+    // 將取得的資料轉成陣列形式
+    $memberRows = $member->fetch(PDO::FETCH_ASSOC);
 
-?>
+    // 將陣列轉成json格式後傳出去
+    echo json_encode($memberRows);
+    break;
+    
+  case "edit":
+
+    //接收xhr送過來的資料 像是性向 / 自我介紹等等...
+    $mem_no = $_POST['mem_no'];
+    $mem_power = $_POST['mem_power'];
+    $mem_state = $_POST['mem_state'];
+    //update
+    $sql = "UPDATE mem  SET mem_state = $mem_state, mem_power = $mem_power WHERE mem.mem_no = $mem_no";
+    //執行sql
+
+    break;
+}
