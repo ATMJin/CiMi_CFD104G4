bef2a92c72322d07b2a9958238e93ae497e7ee25
ATMJin <atmjin0912@protonmail.com>
2022-03-23 00:30:01 +0800

    開啟燈箱

--- src/js/search.js
+++ src/js/search.js
@@ -1,6 +1,7 @@
 const vm = new Vue({
   el: "#searchapp",
   data: {
+    lightbox: null,
     billboards: [],
     articles: [],
     goods: [],
@@ -33,6 +34,21 @@ const vm = new Vue({
         })
         .catch(err => console.log(err));
     },
+    setTargets() {
+      this.lightbox = document.querySelectorAll(".lightbox");
+      // console.log(this.lightbox);
+      this.close = document.querySelectorAll(".close_icon");
+      this.article_box = document.getElementsByClassName("article_box");
+      // let article_like = document.getElementById('article_like');
+      // let article_collect = document.getElementById('article_like');
+    },
+
+    close_lightbox() {
+      this.lightbox[0].style.display = "none";
+      let html = document.getElementsByTagName('html')[0];
+      html.style = "overflow:hidden scroll";
+
+    },
     open_lightbox(a_num) {
       this.lightbox[0].style.display = "block";
       let html = document.getElementsByTagName('html')[0];
@@ -43,6 +59,11 @@ const vm = new Vue({
       this.setCollect(a_num);
       this.get_comment_info();
       // console.log(this.no);
+    },
+    usePostXhr(xhr, send) {
+      xhr.open("post", "phps/article_database.php", true);
+      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
+      xhr.send(send);
     },
     // 送出留言
     send_comment() {
@@ -71,41 +92,182 @@ const vm = new Vue({
       console.log(article_no);
       // 送進資料庫
       let xhr = new XMLHttpRequest();
-      xhr.onload = function () {
+      this.usePostXhr(xhr, `${comment}&${comment_date}&${mem_no}&${article_no}&case=get_comment`)
+
+      xhr.onload = () => {
         console.log(xhr.responseText);
       }
-      xhr.open("post", "phps/article_database.php", true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-      xhr.send(`${comment}&${comment_date}&${mem_no}&${article_no}&case=get_comment`);
       this.open_lightbox(this.a_num);
     },
     get_like_article() {
       let xhr = new XMLHttpRequest();
-      let thisVue = this;
-      xhr.onload = function () {
-        thisVue.like_articles = JSON.parse(xhr.responseText);
-        // console.log(thisVue.like_articles);
-      }
+      this.usePostXhr(xhr, "case=3")
 
-      xhr.open("post", "phps/article_database.php", true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-      xhr.send("case=3");
+      xhr.onload = () => {
+        this.like_articles = JSON.parse(xhr.responseText);
+        // console.log(this.like_articles);
+      }
     },
     get_comment_info() {
       let xhr = new XMLHttpRequest();
-      let thisVue = this;
-      xhr.onload = function () {
-        thisVue.comment_info = JSON.parse(xhr.responseText);
-        // console.log(thisVue.hot_articles);
+      this.usePostXhr(xhr, `article_no=${this.a_num}&case=find_comment_info`)
+
+      xhr.onload = () => {
+        this.comment_info = JSON.parse(xhr.responseText);
+        // console.log(this.hot_articles);
         // thisVue.article_comment_info=thisVue.comment_info;
         // console.log(thisVue.comment_info);
 
       }
+    },
+    loginAlert() {
+      swal("登入才能進行操作哦! 立即前往登入頁面??", {
+          buttons: {
+            catch: {
+              text: "否",
+              value: "否",
+            },
+            default: {
+              text: "是",
+              value: "是",
+            },
+          },
+        })
+        .then((value) => {
+          switch (value) {
+            case "是":
+              location.href = "login_new.html"
+              break;
+            case "否":
+              break;
+          }
+        });
+    },
+    login_confirm() {
+      if (sessionStorage.getItem('login') == null) {
+        this.loginAlert()
+      }
+    },
+    add_like() {
+      if (sessionStorage.getItem('login') == null) {
+        this.loginAlert()
+      } else {
+        let article_like_img = document.getElementById('article_like_img');
+        let article_like = document.getElementById("article_like")
 
-      xhr.open("post", "phps/article_database.php", true);
-      xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded")
-      // console.log("a.num", this.a_num);
-      xhr.send(`article_no=${this.a_num}&case=find_comment_info`);
+        if (article_like.dataset.like == "false") {
+          article_like_img.src = "assets/images/icon/like.png";
+          article_like.dataset.like = "true"
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=add_like`)
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        } else {
+          article_like_img.src = "assets/images/icon/like_hov.png";
+          article_like.dataset.like = "false"
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=remove_like`)
+
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        }
+      }
+    },
+    add_collect() {
+      if (sessionStorage.getItem('login') == null) {
+        this.loginAlert()
+      } else {
+
+        let article_collect_img = document.getElementById('article_collect_img');
+        let article_collect = document.getElementById("article_collect")
+
+        if (article_collect.dataset.collect == "false") {
+          article_collect_img.src = "assets/images/icon/save.png";
+          article_collect.dataset.collect = "true";
+
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=add_collect`)
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        } else {
+          article_collect_img.src = "assets/images/icon/save_hov.png";
+          article_collect.dataset.collect = "false";
+
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=remove_collect`)
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        }
+      }
+    },
+    // 按更多
+    more_action() {
+      let article_other_img = document.getElementById('article_other_img');
+      let impeach = document.getElementById('impeach');
+      let article_more = document.getElementById("article_other_function")
+
+      if (article_more.dataset.more == "false") {
+        impeach.style.display = 'block';
+        article_other_img.src = "assets/images/icon/more.png";
+        article_more.dataset.more = "true";
+
+      } else {
+        impeach.style.display = 'none';
+        article_other_img.src = "assets/images/icon/more_hov.png";
+        article_more.dataset.more = "false";
+
+      }
+    },
+    send_impeach() {
+      if (sessionStorage.getItem('login') == null) {
+        this.loginAlert()
+      } else {
+        let impeach_txt = document.getElementById("impeach_txt");
+
+        if (impeach_txt.innerText == "檢舉") {
+          impeach_txt.innerText = "取消檢舉";
+          window.alert('已收到您的檢舉！');
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=add_impeach`)
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        } else {
+          impeach_txt.innerText = "檢舉";
+
+          // 送進資料庫
+          let xhr = new XMLHttpRequest();
+          this.usePostXhr(xhr, `article_no=${this.a_num}&case=remove_impeach`)
+
+          xhr.onload = function () {
+            console.log(xhr.responseText);
+          }
+        }
+      }
+
+    },
+    setCollect(num) {
+      let arr = [1, 2, 7, 12];
+      if (arr.indexOf(num) > -1) {};
+    },
+    login_confirm() {
+      if (sessionStorage.getItem('login') == null) {
+        this.loginAlert()
+      }
     },
   },
   created() {
@@ -113,5 +275,7 @@ const vm = new Vue({
     this.get_like_article();
     this.get_comment_info();
   },
-  mounted() {},
+  mounted() {
+    this.setTargets()
+  },
 })
