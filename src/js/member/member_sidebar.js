617fc4803264054f7d02d59815852549f5e2f9c4
j90313 <j90313@gmail.com>
2022-03-23 14:15:36 +0800

    更新sidebar小東西

--- src/js/member/member_sidebar.js
+++ src/js/member/member_sidebar.js
@@ -1,28 +1,11 @@
 window.addEventListener("load", () => {
-    let xhr = new XMLHttpRequest();
-    xhr.open("get", `phps/member/member_sidebar.php?mem_no=${sessionStorage.getItem('mem_no')}`, true);
-    xhr.send(null);
-
-    xhr.onload = function () {
-        // 接收PHP傳來的資料
-        member = JSON.parse(xhr.responseText);
-        // console.log(xhr.responseText);
-
-        let head = document.querySelectorAll('.member_head_img img')
-        let child = document.querySelectorAll('.my_integral p')
-        let user = document.querySelectorAll('.member_head p')
-
-
-        head[0].src = member.mem_head;
-        user[0].innerText = member.mem_name;
-        child[0].innerText = member.mem_point;
-    }
 
     logout = document.getElementsByClassName('my_logout')
     // console.log(logout);
     logout[0].addEventListener('click', function () {
         sessionStorage.removeItem("login");
         var button = document.querySelector('.my_logout');
+
         function logout() {
             alert('確認要登出嗎');
             if (confirm('確認要登出嗎') == true) {
@@ -35,5 +18,28 @@ window.addEventListener("load", () => {
         location.href = "homepage.html"
     })
 
-    // check();
 })
+
+new Vue({
+    el: '#sidebar',
+    data: {
+        member: []
+    },
+    mounted() {
+        let xhr = new XMLHttpRequest();
+        xhr.open("get", `phps/member/member_sidebar.php?mem_no=${sessionStorage.getItem('mem_no')}`, true);
+        xhr.send(null);
+
+        let vm = this
+        xhr.onload = function () {
+            // 接收PHP傳來的資料
+            vm.member = JSON.parse(xhr.responseText)
+        }
+    },
+    methods: {
+
+    },
+})
+// $('.my_integral').hover(function(){
+//         $('my_integral').append("<p>{{member.mem_point}}</p>")
+// })
