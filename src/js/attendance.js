1f9e95ed09785cc149b8e8afefda6de1a41dbf4a
YiWen-chao <chaoyiwen7@gmail.com>
2022-03-23 12:06:36 +0800

    weekdays簽到

--- src/js/attendance.js
+++ src/js/attendance.js
@@ -1,66 +1,53 @@
-let mem_no = sessionStorage.getItem('mem_no');
 let attendances = document.querySelectorAll(".attendance_item", ".attendance_7days_item");
 let attendances_7days = document.querySelectorAll(".attendance_7days_item");
 let coins_bonus= document.querySelectorAll(".coins_bonus_item");
 let coins = document.querySelectorAll(".coins_item");
 let txt_box = document.querySelectorAll(".txt_box");
 let checks = document.getElementsByClassName("check_item");
-let txt =document.getElementsByClassName("txt_box");
-let close = document.querySelectorAll(".close_icon");
+let txt = document.getElementsByClassName("txt_box");
+let coins_content = document.getElementsByClassName("coins_content");
+let close = document.querySelector(".close_icon");
 let lightbox = document.querySelectorAll(".lightbox");
 let day_list = ['日', '一', '二', '三', '四', '五', '六'];
 let day = new Date().getDay();
+let mem_point_add = 0;
+let mem_last_sign;
 
-let now = new Date();
 
-let year = now.getFullYear();
-let month = now.getMonth() + 1;
-let date = now.getDate();
-let check_day = `${year}-${month}-${date}`;
+let current_date = new Date();
+
+let year_a = current_date.getFullYear();
+let month_a = current_date.getMonth() + 1;
+let date_a = current_date.getDate();
 
 (function monthchange() {
-  month = (month < 10 ? '0' : '') + month;
+  month_a = (month < 10 ? '0' : '') + month_a;
 })();
 
 (function datechange() {
-  date = (date < 10 ? '0' : '') + date;
+  date_a = (date < 10 ? '0' : '') + date_a;
 })();
 
+let check_date = `${year_a}-${month_a}-${date_a}`;
 
 
 
 
-
-// console.log('close');
-function if_show_attendence(){
+function getsign_date(){
     let xhr = new XMLHttpRequest();
-    xhr.onload = function(){
-        let attendence = JSON.parse(xhr.responseText);
-        console.log(xhr.responseText);
-        if (attendence.mem_lastsign == check_day && mem_no !== null) {
-
-            for(let i = 0; i<attendances.length; i++){
-            attendances[i].onclick = function(){
-                coins[i].style.display = "none";
-                txt_box[i].style.display = "none";
-                checks[i].style.display = "block";
-                };
-            }
-            attendances_7days[0].onclick = function(){
-            coins_bonus[0].style.display = "none";
-            txt_box[6].style.display = "none";
-            checks[6].style.display = "block";
+    xhr.onload = function () {
+        let memsign = JSON.parse(xhr.responseText);
+        // console.log(questions[0].optionA);
+   
+    if (memsign[0].mem_lastsign == check_date) {
+        lightbox[0].style.display = "none";
+    } else {
+        lightbox[0].style.display = "block";
     }
-            
-        } else {
-            
-        }
-
-        }
-    xhr.open("get", `phps/attendence?mem_no=${sessionStorage.getItem('mem_no')}.php`, true);
+ }
+    xhr.open("get", `phps/attendence.php?mem_no=${sessionStorage.getItem('mem_no')}`, true);
     xhr.send(null);
-}
-
+};
 
 
 function close_lightbox(){
@@ -68,16 +55,54 @@ function close_lightbox(){
 }
 
 
-
-
 document.write('Today is 星期' + day_list[day]);
 
+function weekdays_click() {
+    if (mem_no == null) {
+        alert("登入或註冊，即可簽到累積點數喔!")
+        window.location.href = "login_new.html";
+    }else {
+        
+        coins[day - 1].style.display = "none";
+        txt_box[day].style.display = "none";
+        checks[day - 1].style.display = "block";
+        mem_point_add = mem_point_add + 1;
 
+        // coins_bonus[0].style.display = "none";
+        // txt_box[6].style.display = "none";
+        // checks[6].style.display = "block";
 
-function init() {
-    
-    close[0].onclick=close_lightbox;
+          $.ajax({
+            url: 'phps/attendence_addpoint.php',
+            data: {mem_no,mem_point_add,check_date},
+            
+            type: 'GET',
+            success(res) {
+                alert(`累積點數!+${mem_point_add}`);
+                
+            },
+
+        
+        });
+    }
+};
+
+function passed_days() {
+    for (let i = 0; i < day - 1; i++) {
+        coins_content[i].style = " background-color: #a3a3a3;"
+    }
 }
 
 
+function init(){
+    passed_days();
+    getsign_date();
+    attendances[day - 1].addEventListener("click", weekdays_click);
+
+    if (day == 0) {
+         attendances_7days[0].addEventListener("click", weekdays_click);
+        };
+    
+    close.onclick = close_lightbox;
+}
 window.addEventListener("load", init, false);
---
+++ src/phps/attendence_addpoint.php
@@ -0,0 +1,26 @@
+<?php 
+try {
+	require_once("connect_cimi.php");
+	//require=>沒有回傳值
+	//執行sql指令並取得pdoStatement
+    $mem_no=$_GET["mem_no"];
+	$mem_point_add = $_GET["mem_point_add"];
+	$check_date = $_GET["check_date"];
+	$sql = "update mem set mem_point = mem_point + {$mem_point_add}, mem_lastsign = '{$check_date}', mem_stacksign = mem_stacksign + 1 where mem_no={$mem_no};";
+
+	//變為陣列型態
+
+
+	
+	if($pdo->exec($sql)){ //exec用於異動資料的時候
+		echo "異動成功~";
+	}else{
+		echo "異動失敗~";
+	}
+
+}catch (PDOException $e) {
+	echo "錯誤行號 : ", $e->getLine(), "<br>";
+	echo "錯誤原因 : ", $e->getMessage(), "<br>";
+}
+
+?>
